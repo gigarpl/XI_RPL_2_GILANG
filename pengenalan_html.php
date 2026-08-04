43de51089e8f233e2441d37a6f2ce2eb4a5987a3
gigarpl <groma3491@gmail.com>
2026-08-04 13:25:36 +0700

    Initial commit

---
+++ pengenalan_html.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <tittle>
+            pengenalan html
+<tittle>
+<head>
+    <body>
+        <h2 align="center">
+            web pengenalan html
+        <h2>
+    </body>
+<html>
